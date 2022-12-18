3a1da860883a0741b84f518ad3376ac5685d93fc
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2022-12-18 15:13:23 +0530

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,24 @@
 import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
- const [value, setValue] = useState('')
- const [className, setClassName] = useState('square')
- const [count ,setCount] = useState(0)
- const [shapeList, setShapeList] = useState([])
-function handleChange(){
-  setClassName(value)
-  setCount(count+1)
-  setShapeList([...shapeList,count])
-}
+  const [value, setValue] = useState('square')
+  const [className, setClassName] = useState('')
+  const [shapeList, setShapeList] = useState([])
+  function handleChange() {
+    setClassName(value)
+    setShapeList([...shapeList, value])
+  }
   return (
     <div id="main">
       <div id="shape-creator">
-        <select onChange={(e)=>{setValue(e.target.value)}}>
+        <select onChange={(e) => { setValue(e.target.value) }}>
           <option value="square">Square</option>
           <option value="circle">Circle</option>
         </select>
         <button onClick={handleChange}>Add shape</button>
       </div>
       <div id="shapes-holder" >
-        {shapeList.map((element,idx)=> {return <div className={className}>{idx}</div>})}
+        {shapeList.map((element, idx) => { return <div className={className}>{idx}</div> })}
       </div>
     </div>
   )
